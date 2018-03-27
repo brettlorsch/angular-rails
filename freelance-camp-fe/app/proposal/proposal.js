82c9d0223ddb179a81d68c00331918f6ca92c087
brett <brettlorsch@gmail.com>
2018-03-26 22:41:47 -0600

    built component that will create new proposals

--- freelance-camp-fe/app/proposal/proposal.js
+++ freelance-camp-fe/app/proposal/proposal.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Proposal = /** @class */ (function () {
-    function Proposal(id, customer, protfolio_url, tools, estimated_hours, hourly_rate, weeks_to_complete, client_email) {
-        if (protfolio_url === void 0) { protfolio_url = 'http://'; }
+    function Proposal(id, customer, portfolio_url, tools, estimated_hours, hourly_rate, weeks_to_complete, client_email) {
+        if (portfolio_url === void 0) { portfolio_url = 'http://'; }
         this.id = id;
         this.customer = customer;
-        this.protfolio_url = protfolio_url;
+        this.portfolio_url = portfolio_url;
         this.tools = tools;
         this.estimated_hours = estimated_hours;
         this.hourly_rate = hourly_rate;
@@ -15,4 +15,5 @@ var Proposal = /** @class */ (function () {
     return Proposal;
 }());
 exports.Proposal = Proposal;
+// 15, 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com'
 //# sourceMappingURL=proposal.js.map
--- freelance-camp-fe/app/proposal/proposal.ts
+++ freelance-camp-fe/app/proposal/proposal.ts
@@ -2,12 +2,13 @@ export class Proposal {
   constructor(
     public id?: number,
     public customer?: string,
-    public protfolio_url: string = 'http://',
+    public portfolio_url: string = 'http://',
     public tools?: string,
     public estimated_hours?: number,
     public hourly_rate?: number,
     public weeks_to_complete?: number,
     public client_email?: string,
-
   ) {}
 }
+
+// 15, 'Abc Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com'
