3e16f9145a216b4e24c9624765c48001fd07eb9a
brett <brettlorsch@gmail.com>
2018-03-28 13:59:56 -0600

    managing API data

--- freelance-camp-fe/app/app.module.js
+++ freelance-camp-fe/app/app.module.js
@@ -12,6 +12,7 @@ var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
 var homepage_component_1 = require("./homepage/homepage.component");
 var documents_component_1 = require("./documents/documents.component");
+var document_service_1 = require("./documents/document.service");
 var proposal_list_component_1 = require("./proposal/proposal-list.component");
 var proposal_new_component_1 = require("./proposal/proposal-new.component");
 var proposal_show_component_1 = require("./proposal/proposal-show.component");
@@ -33,6 +34,9 @@ var AppModule = /** @class */ (function () {
                 proposal_list_component_1.ProposalListComponent,
                 proposal_new_component_1.ProposalNewComponent,
                 proposal_show_component_1.ProposalShowComponent
+            ],
+            providers: [
+                document_service_1.DocumentService
             ],
             bootstrap: [
                 app_component_1.AppComponent
